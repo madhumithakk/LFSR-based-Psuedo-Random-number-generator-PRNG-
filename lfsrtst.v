e34a250a89ce12283c2603c7f967318f9c28cf48
madhumithakk <madhumithakk3@gmail.com>
2026-01-04 20:17:41 +0530

    lfsrtst.v

---
+++ lfsrtst.v
@@ -0,0 +1,24 @@
+module lfsrtst;
+reg clk;
+reg rst;
+reg [3:0] seed;
+reg load;
+wire q;
+lfsr L(q, clk, rst, seed, load);
+initial
+begin
+ clk = 0;
+ load = 0;
+ seed = 0;
+ rst = 0;
+ #10 rst = 1;
+ #10 rst = 0;
+end
+always
+#50 clk = !clk;
+initial begin
+ #100 seed = 4'b0001;
+ load = 1;
+ #100 load = 0;
+end
+endmodule
